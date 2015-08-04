f5f1d60fdcf799f51388ad021ff16cf628bc72da
shanebe <shane@shanebennett.ca>
2015-08-04 10:24:32 -0400

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -5,15 +5,18 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+
+-- Check to see if Database exists, if so, drop this database and create it new
+
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
--- Players table
+-- Players table used for unique player id (primary ket), name and date created 
 CREATE TABLE players (id serial primary key,
-                      name varchar (255) not null,
+                      name text not null,
                       created_at timestamp default current_timestamp);
 
 
--- Matches table
+-- Matches table, holds match id, winner and loser information (related to player id from players table)
 CREATE TABLE matches (id serial primary key,
                       winner int,
                       loser int,
